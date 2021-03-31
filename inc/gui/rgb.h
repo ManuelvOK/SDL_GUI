c36fdd464524fbd8c247a9248dfaa65498460818
manuelthieme <info@manuel-thieme.de>
2021-03-31 15:04:53 +0200

    get rid of overloaded assignment operator in RGB

--- inc/gui/rgb.h
+++ inc/gui/rgb.h
@@ -14,15 +14,6 @@ public:
     unsigned char _b = 0; /**< blue value between 0 and 255 */
     unsigned char _a = 0; /**< apha value between 0 and 255 */
 
-    /** prediefined color codes the object can be instantiated with. */
-    const std::map<std::string, std::array<unsigned char, 4>> _predefinitions = {
-        {"red",   {255,   0,   0, 255}},
-        {"green", {  0, 255,   0, 255}},
-        {"blue",  {  0,   0, 255, 255}},
-        {"white", {255, 255, 255, 255}},
-        {"black", {  0,   0,   0, 255}},
-    };
-
     /** Default constructor */
     RGB() = default;
 
@@ -46,8 +37,6 @@ public:
 
     /** conversion operator for SDL_Color */
     operator SDL_Color() const;
-
-    RGB &operator=(const RGB &rgb);
 
     /**
      * activate color for a given alpha on a given renderer
--- src/gui/rgb.cc
+++ src/gui/rgb.cc
@@ -2,11 +2,20 @@
 
 using namespace SDL_GUI;
 
+/** prediefined color codes the object can be instantiated with. */
+static const std::map<std::string, std::array<unsigned char, 4>> predefinitions = {
+    {"red",   {255,   0,   0, 255}},
+    {"green", {  0, 255,   0, 255}},
+    {"blue",  {  0,   0, 255, 255}},
+    {"white", {255, 255, 255, 255}},
+    {"black", {  0,   0,   0, 255}},
+};
+
 RGB::RGB(std::string color) {
-    if (this->_predefinitions.find(color) == this->_predefinitions.end()) {
+    if (predefinitions.find(color) == predefinitions.end()) {
         return;
     }
-    std::array<unsigned char, 4> rgb_values = this->_predefinitions.at(color);
+    std::array<unsigned char, 4> rgb_values = predefinitions.at(color);
     this->_r = rgb_values[0];
     this->_g = rgb_values[1];
     this->_b = rgb_values[2];
@@ -15,13 +24,6 @@ RGB::RGB(std::string color) {
 
 RGB::operator SDL_Color() const {
     return SDL_Color{this->_r, this->_g, this->_b, this->_a};
-}
-
-RGB &RGB::operator=(const RGB &rgb) {
-    this->_r = rgb._r;
-    this->_g = rgb._g;
-    this->_b = rgb._b;
-    this->_a = rgb._a;
 }
 
 void RGB::activate(SDL_Renderer *renderer) const {
